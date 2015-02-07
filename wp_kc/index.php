c48499014f0adbd0359305bc9c259b2c62ee46cd
mvouillo <uzumaki.25@hotmail.fr>
2015-02-07 12:34:45 +0100

    Dossier wp du thème kit casino
    J'ai modifié la page header.php, index.php et style.css.
    Je t'envoie aussi la base du thème

---
+++ wp_kc/index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		
+		<!-- /section -->
+
+		<div id="devise">
+			<h1>Le casino s'invite chez vous !</h1>
+		</div>
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
